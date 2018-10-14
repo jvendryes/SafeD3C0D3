2128b1221ae75f050b9a8b58d2312ffdee8641d9
Jason Vendryes <jason@chimplabs.com>
2018-10-14 18:02:32 -0400

    Add initial scaffolding

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sdc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'SafeDC0D3';
+}
