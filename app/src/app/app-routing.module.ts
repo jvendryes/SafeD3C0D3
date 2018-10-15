c30bdd75f45eea464584eba65dc6d6fc45b918e9
Jason Vendryes <jason@chimplabs.com>
2018-10-15 12:09:29 -0400

    Set up some basic routes to get started

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: 'decoder',
+      loadChildren: 'src/app/decoder/decoder.module#DecoderModule'
+    },
+    {
+      path: '',
+      redirectTo: '',
+      pathMatch: 'full'
+    },
+    {
+      path: '**',
+      redirectTo: ''
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/decoder/decoder-routing.module.ts
+++ app/src/app/decoder/decoder-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { JwtComponent } from "./jwt/jwt.component";
+import { Base64Component } from "./base64/base64.component";
+
+const routes: Routes = [
+    {
+      path: 'jwt',
+      component: JwtComponent
+    },
+    {
+      path: 'base64',
+      component: Base64Component
+    },
+    {
+      path: '',
+      redirectTo: '/decoder/jwt',
+      pathMatch: 'full'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
