a64fda004d3ff3711f299b5e666c11647090d5d2
Jason Vendryes <jason@chimplabs.com>
2018-10-30 09:46:43 -0600

    Add support for decoding the header and payload of a JWT, minor styling updates

--- app/src/app/decoder/jwt/jwt.component.ts
+++ app/src/app/decoder/jwt/jwt.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Base64DecodePipe} from "../../base64-decode.pipe";
 
 @Component({
   selector: 'sdc-jwt',
@@ -7,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JwtComponent implements OnInit {
 
-  constructor() { }
+  public rawJwt: string = "";
+  public jwt: Object = {};
+  public validJwt: boolean = false;
+
+  constructor() {
+  }
+
+  parseJwt(jwt: any) {
+    if(jwt.target.value.length > 0) {
+      let jwtHeader, jwtPayload: string;
+      let jwtSplit = jwt.target.value.split('.');
+      let base64DecodePipe = new Base64DecodePipe();
+
+      jwtHeader = base64DecodePipe.transform(jwtSplit[0]);
+      jwtPayload = base64DecodePipe.transform(jwtSplit[1]);
+
+      this.jwt['header'] = JSON.stringify(JSON.parse(jwtHeader), null, 2);
+      this.jwt['payload'] = JSON.stringify(JSON.parse(jwtPayload), null, 2);
+
+      if (Object.keys(this.jwt).length > 0) {
+          this.validJwt = true;
+      } else {
+          this.validJwt = false;
+      }
+    } else {
+      this.validJwt = false;
+    }
+  }
 
   ngOnInit() {
   }
