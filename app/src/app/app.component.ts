a549777b4ababa802ca577eace6972ce45447346
jvendryes <jason@chimplabs.com>
2019-03-30 16:32:15 -0600

    Update code to clean up linting errors, removed unused error class code

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'sdc-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title: string = 'SafeD3C0D3';
-    version: string = '0.1.2';
-    navStatus: boolean = false;
+export class AppComponent implements OnInit {
+    title: string;
+    version: string;
+    navStatus: boolean;
 
     toggleNav(): void {
         this.navStatus = !this.navStatus;
     }
 
     closeNav(): void {
+        this.navStatus = false;
+    }
+
+    ngOnInit(): void {
+        this.title = 'SafeD3C0D3';
+        this.version = '0.1.2';
         this.navStatus = false;
     }
 }
--- app/src/app/base64-decode.pipe.ts
+++ app/src/app/base64-decode.pipe.ts
@@ -11,7 +11,7 @@ export class Base64DecodePipe implements PipeTransform {
                 return decodeURIComponent(
                     atob(value)
                         .split('')
-                        .map(function(c) {
+                        .map(c => {
                             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
                         })
                         .join('')
